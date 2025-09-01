b84d39768090a232b3fa680f5fbfbf67f461a955
Kiran Gowda <kiranashgowda007@gmail.com>
2025-09-01 12:16:26 +0530

    Update chat_openrouter.py

--- cybersecurity/analysis/chat_openrouter.py
+++ cybersecurity/analysis/chat_openrouter.py
@@ -7,9 +7,10 @@ from langchain_core.utils.utils import secret_from_env
 class ChatOpenRouter(ChatOpenAI):
     openai_api_key: SecretStr = Field(
         alias="api_key",
-        default_factory=secret_from_env("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")
+        default_factory=secret_from_env("add your api key")
     )
     base_url: str = "https://openrouter.ai/api/v1"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
